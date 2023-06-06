b109032622ba5ce337f5f995a79624a1761ce8ea
Alvintris <100253781+Alvintris@users.noreply.github.com>
2023-06-06 12:55:45 +0700

    Version 1.0
    Player Movement
    Maze level
    Goal trigger
    
    Bug Known still not found

--- Assets/Script/Goal.cs
+++ Assets/Script/Goal.cs
@@ -1,14 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Goal : MonoBehaviour
 {
-    public Canvas canvas;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        canvas.enabled = true;
+        Debug.Log("Berhasil");
     }
 }
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D player_rb;
+    private Vector3 direction;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+        player_rb.velocity = direction * Time.deltaTime * moveSpeed;
+
+    }
+}
